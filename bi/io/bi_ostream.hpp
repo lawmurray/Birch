6e2f2d45c21374a253978067181f8e8274c10ab8
Lawrence Murray <lawrence@indii.org>
2017-01-13 23:35:05 +0100

    Added RandomType to Birch code generator.

--- bi/io/bi_ostream.hpp
+++ bi/io/bi_ostream.hpp
@@ -45,6 +45,7 @@ public:
   virtual void visit(const ProgParameter* o);
 
   virtual void visit(const ParenthesesType* o);
+  virtual void visit(const RandomType* o);
 
   virtual void visit(const File* o);
   virtual void visit(const Import* o);
