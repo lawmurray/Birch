bdc11fb6bcc2d7d06368f14e38219cc4cfce4689
Lawrence Murray <lawrence@indii.org>
2017-05-10 16:46:41 +0200

    Corrected cast for pointer types.

--- bi/primitive/cast.hpp
+++ bi/primitive/cast.hpp
@@ -10,25 +10,25 @@ namespace bi {
  * Cast away the const-ness of a pointer. Usually used for the @c this
  * pointer to implement the read-write semantics of objects.
  */
-template<class From, class To = From>
-To* cast(const From* o) {
-  return const_cast<To*>(o);
+template<class T>
+T* cast(const T* o) {
+  return const_cast<T*>(o);
 }
 
 /**
  * Cast away the const-ness of a reference. Usually used for passing rvalues
  * to assignment operator overloads requiring lvalue reference argument.
  */
-template<class From, class To = From>
-To& cast(const From& o) {
-  return const_cast<To&>(o);
+template<class T>
+T& cast(const T& o) {
+  return const_cast<T&>(o);
 }
 
 /**
  * Simultaneously cast a shared pointer to a derived type and remove its
  * constness.
  */
-template<class From, class To = From>
+template<class To, class From>
 std::shared_ptr<To> cast(const std::shared_ptr<const From>& o) {
   return std::dynamic_pointer_cast<To>(std::const_pointer_cast<From>(o));
 }
