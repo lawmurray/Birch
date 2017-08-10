6075913cddab69bf3747ef91f37e7c0939539b00
Lawrence Murray <lawrence@indii.org>
2017-08-10 18:10:11 +0200

    Documentation comments (/** ... */) now caught by lexer and stored in the Location objects associated with statements.

--- bi/common/Location.cpp
+++ bi/common/Location.cpp
@@ -4,8 +4,12 @@
 #include "bi/common/Location.hpp"
 
 bi::Location::Location(File* file, const int firstLine, const int lastLine,
-    const int firstCol, const int lastCol) :
-    file(file), firstLine(firstLine), lastLine(lastLine), firstCol(firstCol), lastCol(
-        lastCol) {
+    const int firstCol, const int lastCol, const std::string& doc) :
+    file(file),
+    firstLine(firstLine),
+    lastLine(lastLine),
+    firstCol(firstCol),
+    lastCol(lastCol),
+    doc(doc) {
   //
 }
--- bi/common/Location.hpp
+++ bi/common/Location.hpp
@@ -3,6 +3,8 @@
  */
 #pragma once
 
+#include <string>
+
 namespace bi {
 class File;
 
@@ -14,9 +16,17 @@ class File;
 struct Location {
   /**
    * Constructor.
+   *
+   * @param file File.
+   * @param firstLine Starting line number.
+   * @param lastLine Ending line number.
+   * @param firstCol Starting column number.
+   * @param lastCol Ending column number.
+   * @param doc Immediately preceding documentation comment, if any.
    */
   Location(File* file = nullptr, const int firstLine = 0, const int lastLine =
-      0, const int firstCol = 0, const int lastCol = 0);
+      0, const int firstCol = 0, const int lastCol = 0,
+      const std::string& doc = "");
 
   /**
    * File.
@@ -27,5 +37,10 @@ struct Location {
    * Location in file.
    */
   int firstLine, lastLine, firstCol, lastCol;
+
+  /**
+   * Preceding documentation comment.
+   */
+  std::string doc;
 };
 }
