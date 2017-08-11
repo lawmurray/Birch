9748f3bdeca51a4b89668f13c75df0aabdc2fbf1
Lawrence Murray <lawrence@indii.org>
2017-08-11 11:03:01 +0200

    Tidied up Doxygen output a bit.

---
+++ bi/doxygen.hpp
@@ -0,0 +1,38 @@
+/**
+ * @mainpage Birch Developer Reference
+ *
+ * Birch is a compiled, imperative, object-oriented, and probabilistic
+ * programming language. The latter is its primary research concern.
+ *
+ * It consists of three components:
+ *
+ * @li the @c birch driver program,
+ * @li the @c birchc compiler program, and
+ * @li the @c libbirch.* compiler library and associated header files.
+ *
+ * This documentation relates to these components and is only of
+ * interest to @em developers of the above; @em users will be more
+ * interested in the standard library, documented separately.
+ *
+ * The @em Modules page is a good place to start.
+ *
+ * @defgroup driver Driver program
+ *
+ * @defgroup compiler Compiler program
+ * @defgroup compiler_common Common
+ * @ingroup compiler
+ * @defgroup compiler_expression Expressions
+ * @ingroup compiler
+ * @defgroup compiler_statement Statements
+ * @ingroup compiler
+ * @defgroup compiler_type Types
+ * @ingroup compiler
+ * @defgroup compiler_io Code generation
+ * @ingroup compiler
+ * @defgroup compiler_visitor Visitors
+ * @ingroup compiler
+ * @defgroup compiler_exception Exceptions
+ * @ingroup compiler
+ *
+ * @defgroup library Compiler library
+ */
