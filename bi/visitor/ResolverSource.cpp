9946c07553f65bc3043c9dc1fde3c870c97847a1
Lawrence Murray <lawrence@indii.org>
2017-09-27 13:14:39 +0200

    Updated behaviour of right-tilde arrow operator (~>) to support non-delay types on the left.

--- bi/visitor/ResolverSource.cpp
+++ bi/visitor/ResolverSource.cpp
@@ -250,21 +250,16 @@ bi::Statement* bi::ResolverSource::modify(Assignment* o) {
     return assign->accept(this);
   } else if (*o->name == "~>") {
     /* replace with equivalent (by definition) code */
-    auto right = new Call(
+    auto observe = new Call(
         new Member(o->right->accept(&cloner),
             new Identifier<Unknown>(new Name("tildeRight"), o->loc), o->loc),
         new Parentheses(o->left->accept(&cloner), o->loc), o->loc);
-    auto left = o->left->accept(&cloner);
-    auto assign = new Assignment(left, new Name("<-"), right, o->loc);
     if (currentYieldType) {
-      auto yield = new Yield(
-          new Member(o->left->accept(&cloner),
-              new Identifier<MemberVariable>(new Name("w"), o->loc), o->loc),
-          o->loc);
-      auto result = new List<Statement>(assign, yield, o->loc);
-      return result->accept(this);
+      auto yield = new Yield(observe, o->loc);
+      return yield->accept(this);
     } else {
-      return assign->accept(this);
+      auto stmt = new ExpressionStatement(observe, o->loc);
+      return stmt->accept(this);
     }
   } else if (*o->name == "~") {
     /* replace with equivalent (by definition) code */
@@ -433,12 +428,15 @@ bi::Statement* bi::ResolverSource::modify(ExpressionStatement* o) {
     auto call = dynamic_cast<Call*>(o->single);
     if (call && call->type->isFiber()) {
       auto name = new Name();
-      auto var = new LocalVariable(name, o->single->type->accept(&cloner), new EmptyExpression(o->loc), o->single, o->loc);
+      auto var = new LocalVariable(name, o->single->type->accept(&cloner),
+          new EmptyExpression(o->loc), o->single, o->loc);
       auto decl = new ExpressionStatement(var, o->loc);
-      auto query = new Query(new Identifier<LocalVariable>(name, o->loc), o->loc);
+      auto query = new Query(new Identifier<LocalVariable>(name, o->loc),
+          o->loc);
       auto get = new Get(new Identifier<LocalVariable>(name, o->loc), o->loc);
       auto yield = new Yield(get, o->loc);
-      auto loop = new While(new Parentheses(query, o->loc), new Braces(yield, o->loc), o->loc);
+      auto loop = new While(new Parentheses(query, o->loc),
+          new Braces(yield, o->loc), o->loc);
       auto result = new List<Statement>(decl, loop, o->loc);
 
       return result->accept(this);
