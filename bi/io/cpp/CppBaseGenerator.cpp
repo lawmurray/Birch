1fff4d5693d726fa01335432f3739cf65611e163
Lawrence Murray <lawrence@indii.org>
2018-01-19 15:03:12 +0100

    Fiber handles now passed-by-value rather than const-reference in generated C++ code, to ensure that they can be resumed within the function body.

--- bi/io/cpp/CppBaseGenerator.cpp
+++ bi/io/cpp/CppBaseGenerator.cpp
@@ -166,7 +166,14 @@ void bi::CppBaseGenerator::visit(const Nil* o) {
 }
 
 void bi::CppBaseGenerator::visit(const Parameter* o) {
-  middle("const " << o->type << "& " << o->name);
+  if (!o->type->isFiber()) {
+    middle("const ");
+  }
+  middle(o->type);
+  if (!o->type->isFiber()) {
+    middle("& ");
+  }
+  middle(o->name);
   if (!o->value->isEmpty()) {
     middle(" = " << o->value);
   }
