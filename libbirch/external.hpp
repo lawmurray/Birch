ba904fcd89f794a41a8e0fa26df550d278766447
Lawrence Murray <lawrence.murray@uber.com>
2019-06-01 18:42:46 -0700

    Missing from previous commit.

---
+++ libbirch/external.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file
+ *
+ * Includes all external headers.
+ */
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <functional>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <initializer_list>
+#include <atomic>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstddef>
+#include <cmath>
+#include <unistd.h>
+#include <getopt.h>
+#include <dlfcn.h>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+#include <eigen3/unsupported/Eigen/KroneckerProduct>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
