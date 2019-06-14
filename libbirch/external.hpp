2b5ecdd4aaf4458bfa3bec4d4c70273883a209eb
Lawrence Murray <lawrence@indii.org>
2019-06-14 14:35:40 -0700

    Added workaround for use of SSE intrinsics in boost headers when compiling for device targets.

--- libbirch/external.hpp
+++ libbirch/external.hpp
@@ -16,7 +16,6 @@
 #include <sstream>
 #include <iomanip>
 #include <initializer_list>
-#include <atomic>
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
@@ -26,9 +25,19 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <dlfcn.h>
+
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Sparse>
 #include <eigen3/unsupported/Eigen/KroneckerProduct>
+
+/* undefine __SSE2__ as workaround for compile errors for device target due
+ * to use of SSE2 intrinsics in Boost headers */
+#define OLD_SSE2 __SSE2__
+#undef __SSE2__
+#include <boost/math/distributions.hpp>
+#include <boost/math/special_functions.hpp>
+#define __SSE2__ OLD_SSE2
+
 
 #ifdef _OPENMP
 #include <omp.h>
