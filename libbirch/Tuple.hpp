d3417b599e592c38849b63b71295761b08a3011e
Lawrence Murray <lawrence.murray@uber.com>
2019-10-12 15:55:55 -0700

    Updated some docs, and Tuple class with new constructors, copy constructors, move constructors, assignment operators, etc.

--- libbirch/Tuple.hpp
+++ libbirch/Tuple.hpp
@@ -12,98 +12,185 @@ namespace libbirch {
 template<class Arg, class ...Args>
 class Tuple {
   template<class Arg1, class ... Args1> friend class Tuple;
-  public:
-  /**
-   * Constructor.
-   */
-  Tuple() {
-    //
-  }
+public:
+  Tuple() = default;
+  Tuple(const Tuple&) = default;
+  Tuple(Tuple&&) = default;
+  Tuple& operator=(const Tuple&) = delete;
+  Tuple& operator=(Tuple&&) = delete;
 
   /**
-   * Constructor.
+   * Constructor for value type.
    */
-  template<class T = Arg>
-  Tuple(const Arg& arg, Args ... args,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(const Arg& arg, Args ... args) :
       head(arg),
       tail(args...) {
     //
   }
 
   /**
-   * Constructor.
+   * Constructor for value head type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Arg& arg, Args ... args,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Arg& arg, Args ... args) :
       head(arg),
       tail(context, args...) {
     //
   }
 
   /**
-   * Constructor.
+   * Constructor for non-value head type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Arg& arg, Args ... args,
-      typename std::enable_if_t<!is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Arg& arg, Args ... args) :
       head(context, arg),
       tail(context, args...) {
     //
   }
 
   /**
-   * Copy constructor.
+   * Copy constructor for value head type.
    */
-  template<class T = Arg>
-  Tuple(const Tuple<Arg,Args...>& o,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
-      head(o.head),
-      tail(o.tail) {
-    //
-  }
-
-  /**
-   * Copy constructor.
-   */
-  template<class T = Arg>
-  Tuple(Label* context, const Tuple<Arg,Args...>& o,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Tuple& o) :
       head(o.head),
       tail(context, o.tail) {
     //
   }
 
   /**
-   * Copy constructor.
+   * Copy constructor for non-value head type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Tuple<Arg,Args...>& o,
-      typename std::enable_if_t<!is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Tuple& o) :
       head(context, o.head),
       tail(context, o.tail) {
     //
   }
 
   /**
-   * Generic copy assignment.
+   * Move constructor for value head type.
    */
-  template<class Arg1, class ... Args1>
-  Tuple<Arg,Args...>& operator=(const Tuple<Arg1,Args1...>& o) {
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, Tuple&& o) :
+      head(std::move(o.head)),
+      tail(context, std::move(o.tail)) {
+    //
+  }
+
+  /**
+   * Move constructor for non-value head type.
+   */
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, Tuple&& o) :
+      head(context, std::move(o.head)),
+      tail(context, std::move(o.tail)) {
+    //
+  }
+
+  /**
+   * Deep copy constructor for value head type.
+   */
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, Label* label, const Tuple& o) :
+      head(o.head),
+      tail(context, label, o.tail) {
+    //
+  }
+
+  /**
+   * Deep copy constructor for non-value head type.
+   */
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, Label* label, const Tuple& o) :
+      head(context, label, o.head),
+      tail(context, label, o.tail) {
+    //
+  }
+
+  /**
+   * Copy assignment for value type.
+   */
+  template<class Arg1, class ... Args1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(const Tuple<Arg1,Args1...>& o) {
     head = o.head;
     tail = o.tail;
     return *this;
   }
 
   /**
-   * Generic move assignment.
+   * Copy assignment for value head type.
    */
-  template<class Arg1, class ... Args1>
-  Tuple<Arg,Args...>& operator=(Tuple<Arg1,Args1...> && o) {
+  template<class Arg1, class ... Args1, class T = Arg,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, const Tuple<Arg1,Args1...>& o) {
+    head = o.head;
+    tail.assign(context, o.tail);
+    return *this;
+  }
+
+  /**
+   * Copy assignment for non-value head type.
+   */
+  template<class Arg1, class ... Args1, class T = Arg,
+      std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, const Tuple<Arg1,Args1...>& o) {
+    head.assign(context, o.head);
+    tail.assign(context, o.tail);
+    return *this;
+  }
+
+  /**
+   * Move assignment for value type.
+   */
+  template<class Arg1, class ... Args1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(Tuple<Arg1,Args1...>&& o) {
     head = std::move(o.head);
     tail = std::move(o.tail);
     return *this;
+  }
+
+  /**
+   * Move assignment for value head type.
+   */
+  template<class Arg1, class ... Args1, class T = Arg,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, Tuple<Arg1,Args1...>&& o) {
+    head = std::move(o.head);
+    tail.assign(context, std::move(o.tail));
+    return *this;
+  }
+
+  /**
+   * Move assignment for non-value head type.
+   */
+  template<class Arg1, class ... Args1, class T = Arg,
+      std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, Tuple<Arg1,Args1...>&& o) {
+    head.assign(context, std::move(o.head));
+    tail.assign(context, std::move(o.tail));
+    return *this;
+  }
+
+  /**
+   * Copy assignment operator for value type.
+   */
+  template<class Arg1, class ... Args1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& operator=(const Tuple<Arg1,Args1...>& o) {
+    return assign(o);
+  }
+
+  /**
+   * Move assignment operator for value type.
+   */
+  template<class Arg1, class ... Args1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& operator=(Tuple<Arg1,Args1...>&& o) {
+    return assign(std::move(o));
   }
 
   /**
@@ -144,91 +231,169 @@ private:
 
 template<class Arg>
 class Tuple<Arg> {
-  template<class Arg1, class ... Args1> friend class Tuple;
-  public:
-  /**
-   * Constructor.
-   */
-  Tuple() {
-    //
-  }
+  template<class Arg1, class... Args1> friend class Tuple;
+public:
+  Tuple() = default;
+  Tuple(const Tuple&) = default;
+  Tuple(Tuple&&) = default;
+  Tuple& operator=(const Tuple&) = delete;
+  Tuple& operator=(Tuple&&) = delete;
 
   /**
-   * Constructor.
+   * Constructor for value type.
    */
-  template<class T = Arg>
-  Tuple(const Arg& arg,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(const Arg& arg) :
       head(arg) {
     //
   }
 
   /**
-   * Constructor.
+   * Constructor for value type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Arg& arg,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Arg& arg) :
       head(arg) {
     //
   }
 
   /**
-   * Constructor.
+   * Constructor for non-value type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Arg& arg,
-      typename std::enable_if_t<!is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Arg& arg) :
       head(context, arg) {
     //
   }
 
   /**
-   * Copy constructor.
+   * Copy constructor for value type.
    */
-  template<class T = Arg>
-  Tuple(const Tuple<Arg>& o,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(const Tuple& o) :
       head(o.head) {
     //
   }
 
   /**
-   * Copy constructor.
+   * Copy constructor for value type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Tuple<Arg>& o,
-      typename std::enable_if_t<is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Tuple& o) :
       head(o.head) {
     //
   }
 
   /**
-   * Copy constructor.
+   * Copy constructor for non-value type.
    */
-  template<class T = Arg>
-  Tuple(Label* context, const Tuple<Arg>& o,
-      typename std::enable_if_t<!is_value<T>::value,int> = 0) :
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, const Tuple& o) :
       head(context, o.head) {
     //
   }
 
   /**
-   * Generic copy assignment.
+   * Move constructor for value type.
    */
-  template<class Arg1>
-  Tuple<Arg>& operator=(const Tuple<Arg1>& o) {
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Tuple&& o) :
+      head(std::move(o.head)) {
+    //
+  }
+
+  /**
+   * Move constructor for value type.
+   */
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, Tuple&& o) :
+      head(std::move(o.head)) {
+    //
+  }
+
+  /**
+   * Move constructor for non-value type.
+   */
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, Tuple&& o) :
+      head(context, std::move(o.head)) {
+    //
+  }
+
+  /**
+   * Deep copy constructor for value type.
+   */
+  template<class T = Arg, std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple(Label* context, Label* label, const Tuple& o) :
+      head(o.head) {
+    //
+  }
+
+  /**
+   * Deep copy constructor for non-value type.
+   */
+  template<class T = Arg, std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple(Label* context, Label* label, const Tuple& o) :
+      head(context, label, o.head) {
+    //
+  }
+
+  /**
+   * Copy assignment for value type.
+   */
+  template<class Arg1, class T = Arg,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(const Tuple<Arg1>& o) {
     head = o.head;
     return *this;
   }
 
   /**
-   * Generic move assignment.
+   * Copy assignment for non-value type.
    */
-  template<class Arg1>
-  Tuple<Arg>& operator=(Tuple<Arg1> && o) {
+  template<class Arg1, class T = Arg,
+      std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, const Tuple<Arg1>& o) {
+    head.assign(context, o.head);
+    return *this;
+  }
+
+  /**
+   * Move assignment for value type.
+   */
+  template<class Arg1, class T = Arg,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& assign(Tuple<Arg1>&& o) {
     head = std::move(o.head);
     return *this;
+  }
+
+  /**
+   * Move assignment for non-value type.
+   */
+  template<class Arg1, class T = Arg,
+      std::enable_if_t<!is_value<T>::value,int> = 0>
+  Tuple& assign(Label* context, Tuple<Arg1>&& o) {
+    head.assign(context, std::move(o.head));
+    return *this;
+  }
+
+  /**
+   * Copy assignment operator for value type.
+   */
+  template<class Arg1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& operator=(const Tuple<Arg1>& o) {
+    return assign(o);
+  }
+
+  /**
+   * Move assignment operator for value type.
+   */
+  template<class Arg1, class T = Tuple,
+      std::enable_if_t<is_value<T>::value,int> = 0>
+  Tuple& operator=(Tuple<Arg1>&& o) {
+    return assign(std::move(o));
   }
 
   /**
