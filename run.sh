531c4fd2aa2bfbd887382d535c96ac1fe28790dd
Lawrence Murray <lawrence.murray@uber.com>
2019-11-25 20:09:14 -0800

    Updated for changes on filter branch in compiler and standard library.

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+echo "Bayesian linear regression model"
 birch sample --config config/linear_regression.json
+
+echo "Linear-Gaussian state-space model"
 birch sample --config config/linear_gaussian.json
+
+echo "Mixed linear-nonlinear-Gaussian state-space model"
 birch sample --config config/mixed_gaussian.json
+
+echo "SIR Markov model"
 birch sample --config config/sir.json
