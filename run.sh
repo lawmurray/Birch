afe395cb6687158315240f33ca5614b129b5289d
Lawrence Murray <lawrence.murray@uber.com>
2020-04-13 16:06:58 -0700

    Moved input and output files from command-line in run.sh into config files.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Simulating"
-birch sample --config config/simulate.json --input input/simulate.json --output output/simulate.json
+birch sample --config config/simulate.json
 
 echo "Visualizing simulation"
 birch draw --input output/simulate.json --output figs/simulate.pdf
@@ -10,7 +10,7 @@ echo "Creating data set from simulation"
 birch data --input output/simulate.json --output input/filter.json
 
 echo "Running particle filter"
-birch sample --config config/filter.json --input input/filter.json --output output/filter.json
+birch sample --config config/filter.json
 
 echo "Visualizing filter"
 birch draw --input output/filter.json --output figs/filter.pdf
