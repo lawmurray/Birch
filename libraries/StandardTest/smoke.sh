7c42380a811eaabcc96abf056c3567be13546833
Lawrence Murray <lawrence@indii.org>
2020-08-30 18:11:52 -0700

    Fixed missing -N options in standard library test runs.

--- libraries/StandardTest/smoke.sh
+++ libraries/StandardTest/smoke.sh
@@ -1,7 +1,7 @@
 P=4
 N=100
 
-ls bi/basic     | grep '\.bi' | sed 's/.bi$/ -N $N/g' | xargs -t -L 1 -P $P birch
-ls bi/cdf       | grep '\.bi' | sed 's/.bi$/ -N $N/g' | xargs -t -L 1 -P $P birch
-ls bi/conjugacy | grep '\.bi' | sed 's/.bi$/ -N $N/g' | xargs -t -L 1 -P $P birch
-ls bi/pdf       | grep '\.bi' | sed 's/.bi$/ -N $N/g' | xargs -t -L 1 -P $P birch
+ls bi/basic     | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/cdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/conjugacy | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/pdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
