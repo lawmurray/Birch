0feb10ad466bd8d5b0995d17ec3853fc3352f04e
Lawrence Murray <lawrence@indii.org>
2020-08-31 21:05:51 -0700

    Merged StandardTest back into Standard to keep tests with code for package checks. Merged smoke and thorough tests into same jobs in CircleCI to simplify dependencies and workspace persistence.

---
+++ libraries/Standard/test.sh
@@ -0,0 +1,7 @@
+P=4
+N=100000
+
+ls bi/test/basic     | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/test/cdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/test/conjugacy | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls bi/test/pdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
--- libraries/StandardTest/README.md
+++
@@ -1,9 +0,0 @@
-# StandardTest package
-
-Tests for the Birch standard library.
-
-To build, install and run, use:
-
-    birch build
-    birch install
-    ./test
