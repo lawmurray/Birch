e4fc08ad234b628c5f5a063d4261f8b187bfad74
Lawrence Murray <lawrence@indii.org>
2020-09-03 22:18:49 -0700

    Updated file extensions in standard library test scripts.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,7 +1,7 @@
 P=4
 N=100
 
-ls birch/test/basic     | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls birch/test/cdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls birch/test/conjugacy | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls birch/test/pdf       | grep '\.bi' | sed "s/.bi$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls birch/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls birch/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls birch/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls birch/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
