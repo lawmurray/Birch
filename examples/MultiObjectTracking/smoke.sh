e70b46ab32df59ef5b2f6fe3f19399513954108d
Lawrence Murray <lawrence@indii.org>
2020-09-25 23:00:58 -0700

    Minor fixes.

--- examples/MultiObjectTracking/smoke.sh
+++ examples/MultiObjectTracking/smoke.sh
@@ -1,1 +1,1 @@
-birch sample --config config/smoke.json --output output/smoke.json --seed
+birch sample --config config/smoke.json --output output/smoke.json --seed 0
--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,5 +1,5 @@
 P=4
-N=100
+N=10
 
 ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
 ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
