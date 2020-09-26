0b8fbacd7d2add658dbe98d71e001d0074a30878
Lawrence Murray <lawrence@indii.org>
2020-09-25 22:33:07 -0700

    Restored parallelism to standard library tests. Tweaks to smoke test for MultiObjectTracking.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,8 +1,9 @@
-N=10
+P=4
+N=100
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
