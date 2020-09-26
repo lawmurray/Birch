d9b246be6aeaa6002c2c6eb9804da854190172d5
Lawrence Murray <lawrence@indii.org>
2020-09-25 21:29:14 -0700

    Reduced smoke test to 10 samples.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,4 +1,4 @@
-N=100
+N=10
 
 ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 birch
 ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 birch
