d47fc614610782dae1dd7e1091675404850a0bee
Lawrence Murray <lawrence@indii.org>
2020-11-04 21:45:56 -0800

    Reviewed t-distributions, inverse gamma and inverse Wishart distributions to ensure consistency. Tightened error thresholds on several tests and tuned other hyperparameters.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -2,13 +2,13 @@
 set -eo pipefail
 
 P=4
-M=5  # for gradient tests
-N=5  # for sampling tests
+M=10  # for gradient tests
+N=10  # for sampling tests
 
-ls src/test/basic        | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/pdf          | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/pdf          | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy    | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy    | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
-ls src/test/cdf          | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/grad         | grep '\.birch' | sed "s/.birch$/ -N $M/g" | xargs -t -L 1 -P $P birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 -P $P birch
--- libraries/Standard/test.sh
+++ libraries/Standard/test.sh
@@ -5,10 +5,10 @@ P=4
 M=1000    # for gradient tests
 N=100000  # for sampling tests
 
-ls src/test/basic        | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/pdf          | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/pdf          | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy    | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy    | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g" | xargs -t -L 1 -P $P birch
-ls src/test/cdf          | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/grad         | grep '\.birch' | sed "s/.birch$/ -N $M/g" | xargs -t -L 1 -P $P birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 -P $P birch
