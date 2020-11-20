096f78a89cb0de55ccab4e87f34440efd42d5a7b
pranav subramani <pranav0212.s@gmail.com>
2020-11-20 00:28:19 -0700

    removing directories from smoke.sh and test.sh

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -7,7 +7,6 @@ N=100  # for sampling tests
 
 ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
 ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/cdf/utility     | grep '\.birch' | sed "s/.birch$//g" | xargs -t -L 1 -P $P birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
 ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
