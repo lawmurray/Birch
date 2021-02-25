9017f692e44e5d174543d5f3c2b98e4960c3a57f
Lawrence Murray <lawrence@indii.org>
2021-02-24 23:14:23 -0800

    Added Conway-Maxwell-Poisson distribution.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -2,14 +2,13 @@
 set -eo pipefail
 
 P=4
-M=10  # for gradient tests
+M=10   # for gradient tests
 N=100  # for sampling tests
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g" | xargs -t -L 1 -P $P birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 -P $P birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
 ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
 ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 -P $P birch
