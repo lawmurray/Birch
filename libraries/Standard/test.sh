7ea1c6d38edd237a088d72af3bd0708a5a0d231c
Lawrence Murray <lawrence@indii.org>
2021-04-03 17:45:47 -0700

    Standardized multivariate Gaussian to always require covariance as matrix (not scalar or vector). Standardized matrix Gaussian to always require three arguments (mean, among-row covariance, among-column covariance) rather than allowing omission of the second. Parallelized tests internally where possible rather than in smoke.sh and test.sh scripts.

--- libraries/Standard/test.sh
+++ libraries/Standard/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
-P=1
 M=1000    # for gradient tests
 N=100000  # for sampling tests
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 -P $P birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 -P $P birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 -P $P birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 -P $P birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 birch
