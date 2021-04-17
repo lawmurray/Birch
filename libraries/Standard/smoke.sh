1e535cb7156da697bb9c238ee4e59b44ea87ebcd
Lawrence Murray <lawrence@indii.org>
2021-04-16 18:23:53 -0700

    Added some exceptions to prevent smoke tests failing for numerical reasons, rather than on assertion checks as intended.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 
-M=10   # for gradient tests
-N=10   # for sampling tests
+N=10
 B=10
 S=0
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$//g"                                | xargs -t -L 1 birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"                          | xargs -t -L 1 birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $N/g"                          | xargs -t -L 1 birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N -B $B -S $S --lazy false/g" | xargs -t -L 1 birch
 ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N -B $B -S $S --lazy true/g"  | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g"             | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"              | xargs -t -L 1 birch
