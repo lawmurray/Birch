6dda92983941e63bec675eb955357b71b13fdb6e
Lawrence Murray <lawrence@indii.org>
2021-04-16 17:50:30 -0700

    Fix for marginalization of variates that have already been assigned values (recent regression). Updated terminology around t-distributions.

--- libraries/Standard/test.sh
+++ libraries/Standard/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
-M=100   # for gradient tests
-N=100000  # for sampling tests
+N1=100000   # for cdf tests
+N2=1000     # for gradient tests
+N3=10000    # for pdf tests
+N4=100000  # for conjugacy tests
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"              | xargs -t -L 1 birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $M/g"              | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g" | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"  | xargs -t -L 1 birch
+ls src/test/basic     | grep '\.birch' | sed "s/.birch$//g"                     | xargs -t -L 1 birch
+ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N1/g"              | xargs -t -L 1 birch
+ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $N2/g"              | xargs -t -L 1 birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N3 --lazy false/g" | xargs -t -L 1 birch
+ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N3 --lazy true/g"  | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N4 --lazy false/g" | xargs -t -L 1 birch
+ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N4 --lazy true/g"  | xargs -t -L 1 birch
