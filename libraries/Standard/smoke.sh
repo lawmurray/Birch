8d133cb5f9796ba9420c0fbf101eb36a1a443c3f
Lawrence Murray <lawrence@indii.org>
2021-04-18 18:41:45 -0700

    Replaced use of xargs in test scripts to support BusyBox (e.g. in Alpine Linux containers).

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -5,10 +5,10 @@ N=10
 B=10
 S=0
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$//g"                                | xargs -t -L 1 birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N/g"                          | xargs -t -L 1 birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $N/g"                          | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N -B $B -S $S --lazy false/g" | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N -B $B -S $S --lazy true/g"  | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy false/g"             | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N --lazy true/g"              | xargs -t -L 1 birch
+ls src/test/basic     | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$//g"                                | bash -v
+ls src/test/cdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N/g"                          | bash -v
+ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N/g"                          | bash -v
+ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N -B $B -S $S --lazy false/g" | bash -v
+ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N -B $B -S $S --lazy true/g"  | bash -v
+ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N --lazy false/g"             | bash -v
+ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N --lazy true/g"              | bash -v
--- libraries/Standard/test.sh
+++ libraries/Standard/test.sh
@@ -6,10 +6,10 @@ N2=1000     # for gradient tests
 N3=10000    # for pdf tests
 N4=100000  # for conjugacy tests
 
-ls src/test/basic     | grep '\.birch' | sed "s/.birch$//g"                     | xargs -t -L 1 birch
-ls src/test/cdf       | grep '\.birch' | sed "s/.birch$/ -N $N1/g"              | xargs -t -L 1 birch
-ls src/test/grad      | grep '\.birch' | sed "s/.birch$/ -N $N2/g"              | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N3 --lazy false/g" | xargs -t -L 1 birch
-ls src/test/pdf       | grep '\.birch' | sed "s/.birch$/ -N $N3 --lazy true/g"  | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N4 --lazy false/g" | xargs -t -L 1 birch
-ls src/test/conjugacy | grep '\.birch' | sed "s/.birch$/ -N $N4 --lazy true/g"  | xargs -t -L 1 birch
+ls src/test/basic     | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$//g"                     | bash -v
+ls src/test/cdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N1/g"              | bash -v
+ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N2/g"              | bash -v
+ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N3 --lazy false/g" | bash -v
+ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N3 --lazy true/g"  | bash -v
+ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N4 --lazy false/g" | bash -v
+ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N4 --lazy true/g"  | bash -v
