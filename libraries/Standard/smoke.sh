8c50026b918d5aa8101831dc9c3318a3ede2282c
Lawrence Murray <lawrence@indii.org>
2021-04-30 20:04:25 -0700

    Extended gradient tests to include gradients with respect to all Randoms, not just the last. Fixed errors revealed.

--- libraries/Standard/smoke.sh
+++ libraries/Standard/smoke.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-set -eov pipefail
+#set -eov pipefail
+set -v
 
-N=10
-B=10
+N=4
+B=4
 S=0
 
 eval "`ls src/test/basic     | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$//g"`"
 eval "`ls src/test/cdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N/g"`"
-eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N/g"`"
+eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N --backward false/g"`"
+eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N --backward true/g"`"
 eval "`ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N -B $B -S $S --lazy false/g"`"
 eval "`ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N -B $B -S $S --lazy true/g"`"
 eval "`ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N --lazy false/g"`"
--- libraries/Standard/test.sh
+++ libraries/Standard/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -eov pipefail
 
-N1=100000   # for cdf tests
-N2=1000     # for gradient tests
-N3=10000    # for pdf tests
-N4=100000   # for conjugacy tests
+N1=100000  # for cdf tests
+N2=100     # for gradient tests
+N3=10000   # for pdf tests
+N4=100000  # for conjugacy tests
 
 eval "`ls src/test/basic     | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$//g"`"
 eval "`ls src/test/cdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N1/g"`"
-eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N2/g"`"
+eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N2 --backward false/g"`"
+eval "`ls src/test/grad      | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N2 --backward true/g"`"
 eval "`ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N3 --lazy false/g"`"
 eval "`ls src/test/pdf       | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N3 --lazy true/g"`"
 eval "`ls src/test/conjugacy | grep '\.birch' | sed "s/^/birch /g" | sed "s/.birch$/ -N $N4 --lazy false/g"`"
