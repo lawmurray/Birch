3c3ba0a003199554771a3e38fc84e95bf3617df3
Lawrence Murray <lawrence@indii.org>
2021-05-02 08:45:09 -0700

    Added data preprocessing and visualization to MultiObjectTracking smoke tests for additional code coverage.

--- examples/MultiObjectTracking/smoke.sh
+++ examples/MultiObjectTracking/smoke.sh
@@ -1,1 +1,3 @@
 birch sample --config config/smoke.json --output output/smoke.json --seed 0
+birch draw --input output/smoke.json --output figs/smoke.pdf
+birch data --input output/smoke.json --output output/smoke_data.json
