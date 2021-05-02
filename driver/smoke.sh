d3016f42cc55443e5da946af8d190dbda7dd87fe
Lawrence Murray <lawrence@indii.org>
2021-05-02 09:56:41 -0700

    Added smoke.sh script for driver instead of writing smoke tests into CircleCI config. Expanded to include all online documentation.

---
+++ driver/smoke.sh
@@ -0,0 +1,25 @@
+# online help
+birch help
+birch help init
+birch help audit
+birch help bootstrap
+birch help configure
+birch help build
+birch help install
+birch help uninstall
+birch help dist
+birch help docs
+birch help clean
+birch help help
+
+# hello world package (but can't build without standard library)
+mkdir hello
+cd hello
+birch init
+birch audit
+birch bootstrap
+birch dist
+birch docs
+birch clean
+cd ..
+rm -rf hello
