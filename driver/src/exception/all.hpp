cad40d3478786e1d646c8bbd7fa1cf875f347289
Lawrence Murray <lawrence@indii.org>
2021-05-07 23:34:45 -0700

    Removed obsolete exceptions from driver.

--- driver/src/exception/all.hpp
+++ driver/src/exception/all.hpp
@@ -5,7 +5,3 @@
 
 #include "src/exception/DriverException.hpp"
 #include "src/exception/FileNotFoundException.hpp"
-#include "src/exception/InheritanceLoopException.hpp"
-#include "src/exception/RedefinedException.hpp"
-#include "src/exception/ReturnTypeDeductionException.hpp"
-#include "src/exception/UndefinedException.hpp"
