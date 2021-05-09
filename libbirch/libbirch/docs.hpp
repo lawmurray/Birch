cf318628ae6109354fd05a3a4d4c50fbf4e1f473
Lawrence Murray <lawrence@indii.org>
2021-05-09 08:44:20 -0700

    Removed obsolete introduction from LibBirch docs.

--- libbirch/libbirch/docs.hpp
+++ libbirch/libbirch/docs.hpp
@@ -15,39 +15,17 @@
  * ## Reference counting
  *
  * LibBirch provides reference counting support in combination with lazy deep
- * copy operations, as described in @ref Murray2020 "Murray (2020)". To use
- * this functionality, a class must derive from Any.
- *
- * Reference-counted objects in LibBirch use three counts, rather than
- * the usual two (shared and weak), in order to support these lazy deep copy
- * operations. The counts are:
- *
- *   - a *shared* count,
- *   - a *weak* count, and
- *   - a *memo* count.
- *
- * The shared and weak counts behave as normal. The memo count is used for
- * keys in the memos used to bookkeep lazy deep copy operations.
- *
- * The movement of the three counts triggers the following operations:
- *
- *   -# When the shared count reaches zero, the object is *destroyed*.
- *   -# If the weak and memo weak counts reach 0, the object is *deallocated*.
- *
- * Shared and Weak pointers determine which objects are reachable from the
- * user program. Memo uses the third type internally for keys: it surrenders
- * these during its own cleanup operations if no other shared or weak pointers
- * exist to an object.
+ * copy operations. This was originally described in @ref Murray2020
+ * "Murray (2020)" but the algorithm has since been substantially updated. It
+ * will be properly documented in future.
  *
  * ## Cycle collection
  *
- * Shared and Weak pointers are can be insufficient, and user-programmed logic
- * to tear down data structures can be problematic with lazy deep copy
- * operations: it can end up creating new objects for the purpose of
- * destroying them. Furthermore, Memo can induce reference cycles that would
- * not otherwise exist in the user program.
- *
- * For this reason, LibBirch provides the cycle collection algorithm after
+ * For dealing with reference cycles, weak references can be insufficient, and
+ * user-programmed logic to tear down data structures can be problematic with
+ * lazy deep copy operations: it can end up creating new objects for the
+ * purpose of destroying them. For this reason, LibBirch provides the cycle
+ * collection algorithm after
  * @ref Bacon2001 "Bacon & Rajan (2001)", with some minor adaptations.
  *
  * ## References
