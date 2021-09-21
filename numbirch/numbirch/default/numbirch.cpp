ab89cc63a15a569cb7145541410168b25bccd2f0
Lawrence Murray <lawrence@indii.org>
2021-09-21 15:37:57 -0700

    Potential workaround for undefined symbol when running on Alpine: explicitly specifying namespace on function definition instead of wrapping in namespace block.

--- numbirch/numbirch/default/numbirch.cpp
+++ numbirch/numbirch/default/numbirch.cpp
@@ -4,32 +4,30 @@
 #include "numbirch/numbirch.hpp"
 #include "numbirch/default/numbirch.hpp"
 
-namespace numbirch {
-
-void init() {
+void numbirch::init() {
   /* older compiler versions that do not support thread-safe static local
    * variable initialization require Eigen to initialize such variables before
    * entering a parallel region */
   Eigen::initParallel();
 }
 
-void term() {
+void numbirch::term() {
   //
 }
 
-void* malloc(const size_t size) {
+void* numbirch::malloc(const size_t size) {
   return std::malloc(size);
 }
 
-void* realloc(void* ptr, const size_t size) {
+void* numbirch::realloc(void* ptr, const size_t size) {
   return std::realloc(ptr, size);
 }
 
-void free(void* ptr) {
+void numbirch::free(void* ptr) {
   std::free(ptr);
 }
 
-void memcpy(void* dst, const size_t dpitch, const void* src,
+void numbirch::memcpy(void* dst, const size_t dpitch, const void* src,
     const size_t spitch, const size_t width, const size_t height) {
   if (dpitch == width && spitch == width) {
     std::memcpy(dst, src, width*height);
@@ -38,8 +36,6 @@ void memcpy(void* dst, const size_t dpitch, const void* src,
   }
 }
 
-void wait() {
+void numbirch::wait() {
   //
-}
-
 }
