ebb9c8eb903885d71b1b76ac454a2665f2f6a072
Lawrence Murray <lawrence@indii.org>
2021-09-23 18:55:42 -0700

    Seed --seed command line option for all example test scripts.

--- examples/Ecology/test.sh
+++ examples/Ecology/test.sh
@@ -1,1 +1,1 @@
-birch sample --config config/sparrow.json --output output/test.json
+birch sample --config config/sparrow.json --output output/test.json --seed 0
--- examples/VectorBorneDisease/test.sh
+++ examples/VectorBorneDisease/test.sh
@@ -1,1 +1,1 @@
-birch sample --config config/yap_dengue.json --output output/test.json --seed 8
+birch sample --config config/yap_dengue.json --output output/test.json --seed 0
