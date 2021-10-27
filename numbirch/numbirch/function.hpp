c87b31c45f341176239dbb8d4b92d6f2eae87b34
Lawrence Murray <lawrence@indii.org>
2021-10-26 20:42:55 -0700

    Revisions to special functions.

--- numbirch/numbirch/function.hpp
+++ numbirch/numbirch/function.hpp
@@ -58,50 +58,30 @@ HOST_DEVICE T lbeta(const T x, const T y) {
 }
 
 template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
-HOST_DEVICE T lchoose(const int x, const int y) {
-  if (y == 0 || y == x) {
-    return T(0);
-  } else if (y == 1 || y == x - 1) {
-    return std::log(T(x));
-  } else if (y < x - y) {
-    return -std::log(T(y)) - lbeta(T(y), T(x - y + 1));
-  } else {
-    return -std::log(T(x - y)) - lbeta(T(y + 1), T(x - y));
-  }
-}
-
-template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
-HOST_DEVICE pair<T> lchoose_grad(const T d, const int x, const int y) {
-  T dx, dy;
-  if (y == 0 || y == x) {
-    dx = T(0);
-    dy = T(0);
-  } else if (y == 1 || y == x - 1) {
-    dx = T(1)/x;
-    dy = T(0);
-  } else if (y < x - y) {
-    dx = -digamma(T(x - y + 1)) + digamma(T(x + 1));
-    dy = -T(1)/y - digamma(T(y)) + digamma(T(x - y + 1));
-  } else {
-    dx = -T(1)/(x - y) - digamma(T(x - y)) + digamma(T(x + 1));
-    dy = T(1)/(x - y) - digamma(T(y + 1)) + digamma(T(x - y));
-  }
-  return pair<T>{d*dx, d*dy};
-}
-
-template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
 HOST_DEVICE T lfact(const int x) {
-  return std::lgamma(T(x) + T(1));
+  return std::lgamma(T(x + 1));
 }
 
 template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
 HOST_DEVICE T lfact_grad(const T d, const int x) {
-  return digamma(T(x) + T(1));
+  return d*digamma(T(x + 1));
+}
+
+template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
+HOST_DEVICE T lchoose(const int x, const int y) {
+  return lfact<T>(x) - lfact<T>(y) - lfact<T>(x - y);
+}
+
+template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
+HOST_DEVICE pair<T> lchoose_grad(const T d, const int x, const int y) {
+  T dx = lfact_grad<T>(d, x) - lfact_grad<T>(d, x - y);
+  T dy = -lfact_grad<T>(d, y) + lfact_grad<T>(d, x - y);
+  return pair<T>{dx, dy};
 }
 
 template<class T, std::enable_if_t<std::is_floating_point<T>::value,int> = 0>
 HOST_DEVICE T lgamma(const T x, const int y) {
-  T z = T(0.25)*(y*(y - 1))*std::log(T(PI));
+  T z = T(0.25)*(y*T(y - 1))*std::log(T(PI));
   for (int i = 1; i <= y; ++i) {
     z += std::lgamma(x + T(0.5)*(1 - i));
   }
