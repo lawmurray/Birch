37e67e92731b052ab0e0c175b356ab66197f7846
Lawrence Murray <lawrence@indii.org>
2021-11-23 18:01:35 -0800

    Added some feature checks for device in init() for CUDA backend.

--- numbirch/numbirch/cuda/cuda.cpp
+++ numbirch/numbirch/cuda/cuda.cpp
@@ -11,7 +11,17 @@ thread_local cudaStream_t stream = 0;
 void cuda_init() {
   #pragma omp parallel
   {
+    int value = 0;
     CUDA_CHECK(cudaGetDevice(&device));
+
+    /* check device support */
+    CUDA_CHECK(cudaDeviceGetAttribute(&value,
+        cudaDevAttrConcurrentKernels, device));
+    assert(value && "device with concurrent kernel support required");
+    CUDA_CHECK(cudaDeviceGetAttribute(&value,
+        cudaDevAttrConcurrentManagedAccess, device));
+    assert(value && "device with concurrent managed memory support required");
+
     CUDA_CHECK(cudaDeviceSetCacheConfig(cudaFuncCachePreferShared));
     CUDA_CHECK(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
   }
