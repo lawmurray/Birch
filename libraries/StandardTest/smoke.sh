cd596ba5665ee9ab2021fa07f453596b559b399a
Lawrence Murray <lawrence@indii.org>
2021-11-24 22:29:28 -0800

    Tweaked parameters for smoke test.

--- libraries/StandardTest/smoke.sh
+++ libraries/StandardTest/smoke.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -eov pipefail
 
-N=10
+N1=100  # for cdf tests
+N2=10   # for gradient tests
+N3=100  # for pdf tests
+N4=100  # for conjugacy tests
 B=100
 S=0
 
-eval "`grep -r "program test_basic_" src     | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1/"                                | sort`"
-eval "`grep -r "program test_cdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N/"                          | sort`"
-eval "`grep -r "program test_grad_" src      | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N --backward false/"         | sort`"
-eval "`grep -r "program test_grad_" src      | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N --backward true/"          | sort`"
-eval "`grep -r "program test_pdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N -B $B -S $S --lazy false/" | sort`"
-eval "`grep -r "program test_pdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N -B $B -S $S --lazy true/"  | sort`"
-eval "`grep -r "program test_conjugacy_" src | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N --lazy false/"             | sort`"
-eval "`grep -r "program test_conjugacy_" src | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N --lazy true/"              | sort`"
+eval "`grep -r "program test_basic_" src     | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1/"                                 | sort`"
+eval "`grep -r "program test_cdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N1/"                          | sort`"
+eval "`grep -r "program test_grad_" src      | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N2 --backward false/"         | sort`"
+eval "`grep -r "program test_grad_" src      | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N2 --backward true/"          | sort`"
+eval "`grep -r "program test_pdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N3 -B $B -S $S --lazy false/" | sort`"
+eval "`grep -r "program test_pdf_" src       | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N3 -B $B -S $S --lazy true/"  | sort`"
+eval "`grep -r "program test_conjugacy_" src | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N4 --lazy false/"             | sort`"
+eval "`grep -r "program test_conjugacy_" src | sed -E "s/^.*program ([A-Za-z0-9_]+).*$/birch \1 -N $N4 --lazy true/"              | sort`"
