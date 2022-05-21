35fc3c8b5968e37114af6b0a127d73c1d3d46aea
Lawrence Murray <lawrence@indii.org>
2022-05-21 15:02:37 -0700

    Removed obsolete info variable.

--- numbirch/numbirch/cuda/cusolver.cpp
+++ numbirch/numbirch/cuda/cusolver.cpp
@@ -9,12 +9,10 @@ namespace numbirch {
 
 thread_local cusolverDnHandle_t cusolverDnHandle;
 thread_local cusolverDnParams_t cusolverDnParams;
-thread_local int* info = nullptr;
 
 void cusolver_init() {
   #pragma omp parallel
   {
-    CUDA_CHECK(cudaMallocHost(&info, sizeof(int)));
     CUSOLVER_CHECK(cusolverDnCreate(&cusolverDnHandle));
     CUSOLVER_CHECK(cusolverDnSetStream(cusolverDnHandle, stream));
     CUSOLVER_CHECK(cusolverDnCreateParams(&cusolverDnParams));
@@ -26,7 +24,6 @@ void cusolver_term() {
   {
     CUSOLVER_CHECK(cusolverDnDestroyParams(cusolverDnParams));
     CUSOLVER_CHECK(cusolverDnDestroy(cusolverDnHandle));
-    CUDA_CHECK(cudaFreeHost(info));
   }
 }
 
