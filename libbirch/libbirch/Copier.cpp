087cf88211ebe34ef705e124400bfe1aefd8282c
Lawrence Murray <lawrence@indii.org>
2022-07-31 14:15:10 -0700

    Fixed bug in Copier from last commit.

--- libbirch/libbirch/Copier.cpp
+++ libbirch/libbirch/Copier.cpp
@@ -6,10 +6,16 @@
 #include "libbirch/Any.hpp"
 
 libbirch::Any* libbirch::Copier::visitObject(Any* o) {
-  auto value = m.get(o);
-  if (!value) {
+  auto& value = m.get(o);
+  if (value) {
+    return value;
+  } else {
     value = o->copy_();
-    value->accept_(*this);
+
+    /* copy the value into a non-reference, as the reference may be
+     * invalidated if m is resized during the call to accept_() below */
+    Any* result = value;
+    result->accept_(*this);
+    return result;
   }
-  return value;
 }
