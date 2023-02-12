2309d874248aa0ab02fb2cb349a1e8ab80c87909
Lawrence Murray <lawrence@indii.org>
2023-02-12 09:36:32 -0800

    Removed absolute URLs in Homebrew formulae. [skip ci]

--- libraries/Standard/birch-standard.rb
+++ libraries/Standard/birch-standard.rb
@@ -1,7 +1,7 @@
 class BirchStandard < Formula
   desc "Standard library of the Birch probabilistic programming language"
   homepage "https://birch.sh"
-  url "https://raw.githubusercontent.com/lawmurray/homebrew-birch/master/birch-standard-0.0.0.tar.gz"
+  url "birch-standard-0.0.0.tar.gz"
   version "0.0.0"
   sha256 "a70676d48ff63b0211d1bac767fd160f42ff80a5ced0e51fd0b457f6f7aa8c33"
   license "Apache-2.0"
