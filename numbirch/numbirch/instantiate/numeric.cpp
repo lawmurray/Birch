e521009191b5b7d6d531ba9de54bf3ada2fe58b8
Lawrence Murray <lawrence@indii.org>
2023-03-24 22:28:20 -0700

    Fixed instantiation of solve functions.

--- numbirch/numbirch/instantiate/numeric.cpp
+++ numbirch/numbirch/instantiate/numeric.cpp
@@ -11,7 +11,15 @@
 
 #define BINARY_MATRIX(f) \
     template Array<real,2> f<Array<real,0>,int>(const Array<real,2>&, const Array<real,0>&); \
-    template Array<real,2> f<real,int>(const Array<real,2>&, const real&);
+    template Array<real,2> f<real,int>(const Array<real,2>&, const real&); \
+    template Array<real,2> f##_grad1<Array<real,0>,int>(const Array<real,2>& g, \
+        const Array<real,2>& B, const Array<real,2>& L, const Array<real,0>& y); \
+    template Array<real,2> f##_grad1<real,int>(const Array<real,2>& g, \
+        const Array<real,2>& B, const Array<real,2>& L, const real& y); \
+    template Array<real,0> f##_grad2<Array<real,0>,int>(const Array<real,2>& g, \
+        const Array<real,2>& B, const Array<real,2>& L, const Array<real,0>& y); \
+    template Array<real,0> f##_grad2<real,int>(const Array<real,2>& g, \
+        const Array<real,2>& B, const Array<real,2>& L, const real& y);
 
 namespace numbirch {
 BINARY_MATRIX(cholsolve)
