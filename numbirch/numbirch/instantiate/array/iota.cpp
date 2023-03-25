1744f460c7182dada58511c3f0708d8e8feb7e35
Lawrence Murray <lawrence@indii.org>
2023-03-24 22:00:59 -0700

    Fixed instantiation of iota() function.

--- numbirch/numbirch/instantiate/array/iota.cpp
+++ numbirch/numbirch/instantiate/array/iota.cpp
@@ -14,16 +14,11 @@
     IOTA_FIRST(f, int) \
     IOTA_FIRST(f, bool)
 #define IOTA_FIRST(f, T) \
-    IOTA_SECOND(f, T, int)
-#define IOTA_SECOND(f, T, U) \
-    IOTA_SIG(f, T, U) \
-    IOTA_SIG(f, T, NUMBIRCH_ARRAY(U, 0)) \
-    IOTA_SIG(f, NUMBIRCH_ARRAY(T, 0), U) \
-    IOTA_SIG(f, NUMBIRCH_ARRAY(T, 0), NUMBIRCH_ARRAY(U, 0))
-#define IOTA_SIG(f, T, U) \
-    template Array<value_t<implicit_t<T,U>>,1> f<T,U,int>(const T& x, \
-        const U& y, const int n); \
-    template Array<real,0> f##_grad<T,U,int>(const Array<real,1>& g, \
+    IOTA_SIG(f, T) \
+    IOTA_SIG(f, NUMBIRCH_ARRAY(T, 0))
+#define IOTA_SIG(f, T) \
+    template Array<value_t<T>,1> f<T,int>(const T& x, const int n); \
+    template Array<real,0> f##_grad<T,int>(const Array<real,1>& g, \
         const Array<value_t<T>,1>& y, const T& x, const int n);
 
 namespace numbirch {
